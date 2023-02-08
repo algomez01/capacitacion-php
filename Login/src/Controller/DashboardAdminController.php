dda97359b341b614686a5672094f0904f681112d
IsaacG47 <isaac.gomez@sasf.net>
2023-02-08 12:04:18 -0500

    Commit CRUD Login

--- Login/src/Controller/DashboardAdminController.php
+++ Login/src/Controller/DashboardAdminController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/dashboard/admin')]
+#[Route('user')]
 class DashboardAdminController extends AbstractController
 {
     #[Route('/', name: 'app_dashboard_admin_index', methods: ['GET'])]
